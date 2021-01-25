4a5cfd818bc84642369c2666cafbbe83b50c6dfe
Nathan Hites <nhites@smu.edu>
2021-01-25 13:23:08 -0600

    Increases remove_encounter functionality to target specific rows of the encounter table. Cleaned up code and updated README

--- PTU_RNG_Bot.py
+++ PTU_RNG_Bot.py
@@ -1,27 +1,29 @@
 import random
 
 class PTU_RNG_Bot:
-    def __init__(self, types, type_matrix):
+    def __init__(self, types, encounter_table):
         self.types = types
-        self.type_matrix = type_matrix
-        self.type_indices = range(0, len(type_matrix))
+        for mon_type in types:
+            mon_type = mon_type.lower().capitalize() #Protects against "Water" vs "water" vs "wAtEr" mismatches
+        self.encounter_table = encounter_table
+        self.type_indices = range(0, len(encounter_table))
         self.length_of_each_type = []
         for i in self.type_indices:
-            self.length_of_each_type.append(len(type_matrix[i]))
+            self.length_of_each_type.append(len(encounter_table[i]))
         self.initialize_common_rarities()
             
     #Initializate initial rarity of 2 for all Pokemon        
     def initialize_common_rarities(self):
         for i in self.type_indices:
             #initialize rarities of common Pokemon (twice as often as other Pokemon)
-            row = self.type_matrix[i]
-            self.type_matrix[i] = self.type_matrix[i] + row
+            row = self.encounter_table[i]
+            self.encounter_table[i] = self.encounter_table[i] + row
         
         
         
         
         
-    #Creates common encounter within type_matrix pool at "pokemon_type" row - both inputs must be strings
+    #Creates common encounter within encounter_table pool at "pokemon_type" row - both inputs must be strings
     #Rarity of encounter is 1-4 (1 being the least common and 4 being the most common)
     def create_common_encounter(self, pokemon_name, pokemon_type, rarity):
     
@@ -34,21 +36,23 @@ class PTU_RNG_Bot:
             return
     
         #check to see if duplicate Pokemon- no longer necessary because removal exists
-        #if pokemon_name in self.type_matrix:
+        #if pokemon_name in self.encounter_table:
             #print("Pokemon already exists in table. Could not add.")
             #return
     
-        #Find index for Pokemon in the matrix
+        #Find index for Pokemon in the table
         common_mon = pokemon_name.lower().capitalize() #safe for SquIrTle, squirtle, SQUIRTLE, etc- 
                                                        #always results in "Squirtle"
+        common_mon_index = None
         common_mon_type = None
         for key, value in self.types.items():
             if key == pokemon_type.lower().capitalize(): #safe for type, tYpe, Type, etc
-                common_mon_type = value
+                common_mon_type = key
+                common_mon_index = value
   
-        #if invalid bounds, return (as we cannot work on the matrix)
-        if (common_mon_type >= len(self.type_matrix) or common_mon_type == None):
-            print("Invalid Matrix Operation. Could not add.")
+        #if invalid bounds, return (as we cannot work on the table)
+        if (common_mon_index == None or common_mon_index >= len(self.encounter_table)):
+            print("Invalid Table Operation. Could not add.")
             return
         else:
             #Check rarity
@@ -57,15 +61,15 @@ class PTU_RNG_Bot:
             else:
                 for i in range(0, rarity):
                     #Index row and add Pokemon- rare = add only once
-                    self.type_matrix[common_mon_type].append(common_mon)
+                    self.encounter_table[common_mon_index].append(common_mon)
             
             #Sort by alphabetical value after adding mon to make it look pretty            
-            self.type_matrix[common_mon_type].sort()  
-            print("Successfully added {} (Rarity {}) to matrix row {}.".format(pokemon_name, rarity, pokemon_type))
+            self.encounter_table[common_mon_index].sort()  
+            print("Successfully added {} (Rarity {}) to table row {}.".format(common_mon, rarity, common_mon_type))
             
             
             
-    #Creates rare encounter within type_matrix pool at "pokemon_type" row- both inputs must be strings
+    #Creates rare encounter within encounter_table pool at "pokemon_type" row- both inputs must be strings
     #Rarity is locked to "1" for rare encounters
     def create_rare_encounter(self, pokemon_name, pokemon_type):
     
@@ -74,78 +78,103 @@ class PTU_RNG_Bot:
             print("Inputs must be of type 'string.' Could not add.")
             return
     
-        #check to see if duplicate Pokemon
-        if pokemon_name in self.type_matrix:
-            print("Pokemon already exists in matrix. Could not add.")
-            return
+        #check to see if duplicate Pokemon- deprecated because remove functionality added
+        #if pokemon_name in self.encounter_table:
+            #print("Pokemon already exists in table. Could not add.")
+            #return
     
-        #Find index for Pokemon in the matrix
+        #Find index for Pokemon in the table
         rare_mon = pokemon_name.lower().capitalize() #safe for SquIrTle, squirtle, SQUIRTLE, etc
+        rare_mon_index = None
         rare_mon_type = None
         for key, value in self.types.items():
             if key == pokemon_type.lower().capitalize(): #safe for type, tYpe, Type, etc
-                rare_mon_type = value
+                rare_mon_index = value
+                rare_mon_type = key
   
-        #if invalid bounds, return (as we cannot work on the matrix)
-        if (rare_mon_type >= len(self.type_matrix) or rare_mon_type == None):
-            print("Invalid Matrix Operation. Could not add.")
+        #if invalid bounds, return (as we cannot work on the table)
+        if (rare_mon_index == None or rare_mon_index >= len(self.encounter_table)):
+            print("Invalid Table Operation. Could not add.")
             return
         else:
             #Index row and add Pokemon- rare = add only once
-            self.type_matrix[rare_mon_type].append(rare_mon)
+            self.encounter_table[rare_mon_index].append(rare_mon)
             #Sort by alphabetical value after adding mon to make it look pretty
-            self.type_matrix[rare_mon_type].sort() 
-            print("Successfully added rare {} to matrix row {}.".format(pokemon_name, pokemon_type))
+            self.encounter_table[rare_mon_index].sort() 
+            print("Successfully added rare {} to table row {}.".format(rare_mon, rare_mon_type))
             
     #Removes "pokemon_to_remove" num_to_remove times from the encounter table. If the table contains less occurrences of
     #pokemon_to_remove than the specified num_to_remove, all occurrences are removed.
-    def remove_encounter(self, pokemon_to_remove, num_to_remove):
+    def remove_encounter(self, pokemon_to_remove, type_belongs_to, num_to_remove):
         #validate input
-        if (not isinstance(pokemon_to_remove, str) or not isinstance(num_to_remove, int)):
-            print("Inputs must be of type 'string', 'int'. Could not remove.")
+        if (not isinstance(pokemon_to_remove, str) or not isinstance(type_belongs_to, str) or not isinstance(num_to_remove, int)):
+            print("Inputs must be of type 'string', 'string', 'int'. Could not remove.")
+            return
+        if (num_to_remove <= 0):
+            print("Number of entries to remove must be a positive number greater than zero.")
             return
         
         #standardize input (ie: SQUIRTLE, Squirtle, SqUiRTlE, etc will all work properly, as long as it is spelled correctly)
         pokemon_to_remove = pokemon_to_remove.lower().capitalize()
+        type_belongs_to = type_belongs_to.lower().capitalize()
         
-        #check to see whether the Pokemon actually exists in the table
+        #check to see whether the Pokemon and type actually exists in the table
         mon_exists = False
-        for row in self.type_matrix:
+        for row in self.encounter_table:
             if (pokemon_to_remove in row):
                 mon_exists = True
         if (not mon_exists):
-            print(pokemon_to_remove, "does not exist in the encounter table. Could not remove.")
+            print(pokemon_to_remove, "does not exist in the encounter table in any row. Could not remove.")
             return
         
-        #look through encounter table for Pokemon "num_to_remove" times
+        if type_belongs_to not in self.types:
+            print(type_belongs_to,"is not a valid Pokemon Type within the encounter table. Could not remove.")
+            return
+            
+                        
+        #Find index for Pokemon in the table
+        table_index = None
+        for key, value in self.types.items():
+            if key == type_belongs_to:
+                table_index = value    
+            
+        
+        #look through indexed row for Pokemon "num_to_remove" times
         num_removed = 0 #variable to track how many were actually removed, to be printed 
                         #(for the edge case of the table containing less than num_to_remove)
         for i in range(0, num_to_remove):
-            #remove Pokemon from table if it exists there
             removed_true = False #boolean for checking removal on each pass through the table
-            for index, row in enumerate(self.type_matrix):
-                if (pokemon_to_remove in row):
-                    self.type_matrix[index].remove(pokemon_to_remove)
-                    num_removed = num_removed + 1
-                    removed_true = True
-            if not removed_true: #If we didn't remove anything in this pass, there are no more left to remove
+            #remove Pokemon from table row if it exists there
+            if (pokemon_to_remove in self.encounter_table[table_index]):
+                self.encounter_table[table_index].remove(pokemon_to_remove)
+                num_removed = num_removed + 1
+                removed_true = True
+            if not removed_true: #if we didn't remove anything in this pass, there are no more entries left to remove
                                  #so we are done removing
-                if num_removed == 1:
+                if num_removed == 0:
+                    print(pokemon_to_remove,"does not exist within row", type_belongs_to,"of the encounter table.", 
+                          "Could not remove.")
+                    return
+                elif num_removed == 1:
                     print("Successfully removed", num_removed, "instance of", pokemon_to_remove, ".")
                 else:
-                    print("Successfully removed", num_removed, "instance of", pokemon_to_remove, ".")
+                    print("Successfully removed", num_removed, "instances of", pokemon_to_remove, ".")
                 print("There are no more instances of", pokemon_to_remove, "left in the table.")
                 return
+    
         #If we get to the end without running out of Pokemon to remove, just print the number that were removed
-        print("Successfully removed", num_removed, "instance of", pokemon_to_remove, ".")
+        if num_removed == 1:
+            print("Successfully removed", num_removed, "instance of", pokemon_to_remove, "from table row",type_belongs_to,".")
+        else:
+            print("Successfully removed", num_removed, "instances of", pokemon_to_remove, "from row",type_belongs_to,".")
         return
                 
             
             
-    #Generates random Pokemon teams of specified "num_mons" Picks from rows and columns of type_matrix
+    #Generates random Pokemon teams of specified "num_mons" Picks from rows and columns of encounter_table
     def generate_team(self, num_mons):
         if (not isinstance(num_mons, int)):
-            print("Input is not a valid number of Pokemon")
+            print("Input is not a valid number of Pokemon.")
             return
         
         team = []
@@ -153,17 +182,17 @@ class PTU_RNG_Bot:
             #generate num_mons mons
             randtype = random.choice(self.type_indices)
             randmon = random.choice(range(0, self.length_of_each_type[randtype])) #prevents array overflow
-            generated_mon = self.type_matrix[randtype][randmon]
+            generated_mon = self.encounter_table[randtype][randmon]
             team.append(generated_mon)
         print("Generated Team: ", team)
         
     #Generates random Monotype Pokemon teams of specified "num_mons" of a given "type_of_mon"
-    def random_from_type(self, type_of_mon, num_mons):
+    def generate_monotype_team(self, type_of_mon, num_mons):
         if type_of_mon not in self.types.keys():
-            print("Type is not present in matrix")
+            print("Type is not present in table.")
             return
         elif (not isinstance(num_mons, int)):
-            print("Input is not a valid number of Pokemon")
+            print("Input is not a valid number of Pokemon.")
             return
         else:
             #working match
@@ -172,17 +201,17 @@ class PTU_RNG_Bot:
                 #generate num_mons mons
                 type_of = list(self.types.keys()).index(type_of_mon)
                 randmon = random.choice(range(0, self.length_of_each_type[type_of]))
-                generated_mon = self.type_matrix[type_of][randmon]
+                generated_mon = self.encounter_table[type_of][randmon]
                 team.append(generated_mon)
             print("Generated Team: ", team)
         
-    def print_type_matrix(self):
+    def print_encounter_table(self):
         #pair of type_of_mon, row is like ("Dark", 0), etc
         print("---- Encounter Table: ----")
         print("--------------------------")
         for type_of_mon, row in self.types.items():
             print("--------------------------")
-            printable_row = sorted(self.type_matrix[row])
+            printable_row = sorted(self.encounter_table[row])
             print(type_of_mon,": ", printable_row)
         print("--------------------------")
         
--- README.md
+++ README.md
@@ -4,9 +4,11 @@
 
 This program has two main parameters: the "Types" to be randomly picked from (of dimension dim_type), and a Type Matrix (of dimension dim_type by number_mons_per_type). The number of Pokemon in each Type/row does not need to be the same- the classdeals with that automatically.
 
-* <b> Types: </b> Dictionary with key of "Type" and value of zero-indexed integer (ie: type number one is "Dark": 0) *
+* <b> Types: </b> Dictionary with key of "Type" and value of zero-indexed integer (ie: type number one is "Dark": 0).*
 
-* <b> Type Matrix: </b> List of Lists whose first dimension matches the number of entries in the types dictionary. Each entry in the matrix is a Pokemon name. Each Pokemon's name should be entered only <b> once </b> in the array for an equal chance to encounter each Pokemon. Duplicate checking will only be done by the called methods (and not on the initial matrix) *
+* <b> Encounter Table: </b> List of Lists whose first dimension matches the number of entries in the types dictionary. Each entry in the matrix is a Pokemon name. Each Pokemon's name should be entered only <b> once </b> in the array for an equal chance to encounter each Pokemon. ~~Duplicate checking will only be done by the called methods (and not on the initial matrix)~~ Duplicate checking has since been deprecated.*
+
+**It is important to note that the number of rows in the Encounter Table and the number of types in the Types dictionary MUST match up.**
 
 Examples of these two parameters:
 
@@ -19,7 +21,7 @@ types = {
     "Electric": 5
 }
 
-type_matrix = [
+encounter_table = [
 ["Honchkrow", "Mightyena", "Pawniard", "Shiftry", "Sneasel", "Spiritomb"], 
 ["Azumarill", "Clefairy", "Granbull", "Mawile", "Sylveon", "Togetic"], 
 ["Exeggcute", "Gloom", "Grovyle", "Maractus", "Tangela", "Tropius"],
@@ -27,41 +29,55 @@ type_matrix = [
 ["Chinchou", "Poliwhirl", "Psyduck", "Staryu", "Vaporeon"],
 ["Elecktrike", "Emolga", "Luxio", "Manectric", "Rotom", "Stunfisk"] ]
 
+- Encounters
+    <i> Encounters are picked randomly from the bot's internal encounter table based on each Pokemon's internal <b> "rarity" value </b>, with higher rarity values being more often encountered. 
+    - <b> Rarity: </b> The rarity is implicitly defined as the number of times that a Pokemon's name appears within a given row of the encounter table. *Note that Pokemon can have multiple rarities corresponding to two different rows of the table if they have multiple types* (for instance, an Emolga (Electric/Flying) could have Rarity 3 in a Flying row of ["Emolga", "Emolga", "Emolga", "Pidgey", "Spearow"] but only a Rarity 1 in an Electric row of ["Pikachu", "Pichu", "Raichu", "Emolga].</i>
 
 **********************************************************************************************
 
 - Methods
-    - <i> initialize_common_rarities(type_matrix): </i> This method will initialize the rarity of all Pokemon in the type matrix as "common" with a rarity of 2. Rarity is further discussed below.
-        - <b> <i> Parameters: </i> </b>
-            - <b> type_matrix: </b> the Type Matrix that was created as an encounter table. 
-    - <i> create_common_encounter(pokemon_name, pokemon_type, rarity): </i> This method will create a "common" encounter with a given rarity. The higher the "rarity" value, the more times the encounter will be entered into the Type Matrix (for instance, create_common_encounter("Bulbasaur", "Grass", 2) will create two entries of "Bulbasaur" in the "Grass" row of the matrix.
-        - <b> <i> Parameters: </i> </b>
-            - <b> pokemon_name: </b> the name of the Pokemon to be added to the table. The program does not check if you spelled it correctly (but it does check for cases), so please make sure you have spelled it right!
-            - <b> pokemon_type: </b> the type of the Pokemon (where it will be added in the Type Matrix). The program does not check if you spelled it correctly (but it does check for cases), so please make sure you have spelled it right! It will generate an error if the Type that you specified does not exist.
-                - *note:* Types are singly indexed in this, so you could enter Bulbasaur into either Grass or Poison (but not both)
-            - <b> rarity: </b> the relative rarity of the Pokemon. The higher the rarity, the more times the Pokemon will be entered into the Type Matrix.
-        - <b> <i> Sample Output: </i> </b> `Successfully added Bulbasaur (Rarity 2) to matrix row Grass.`
-    - <i> create_rare_encounter(pokemon_name, pokemon_type): </i> This method will create a "rare" encounter with a rarity of 1 (the most uncommon). 
-        - <b> <i> Parameters: </i> </b>
-            - <b> pokemon_name: </b> the name of the Pokemon to be added to the table. The program does not check if you spelled it correctly (but it does check for cases), so please make sure you have spelled it right!
-            - <b> pokemon_type: </b> the type of the Pokemon (where it will be added in the Type Matrix). The program does not check if you spelled it correctly (but it does check for cases), so please make sure you have spelled it right! It will generate an error if the Type that you specified does not exist.
-                - *note:* Types are singly indexed in this, so you could enter Bulbasaur into either Grass or Poison (but not both)
-        - <b> <i> Sample Output: </i> </b> `Successfully added rare Greninja to matrix row Dark.`
-    - <i> generate_team(): </i> This method generates a random team for a Trainer based on the contained type matrix.
+    - <i> initialize_common_rarities(): </i> This method will initialize the rarity of all Pokemon in the encounter table as "common" with a rarity value of 2. If Pokemon already exist within the table, the combined rarity value will instead be doubled (ie: two entries of "Raichu" will cause Raichu to now have a rarity of 4." Rarity is discussed above.
+        - <i> Note: This method is called implicitly upon construction and does not need to be called again unless desired. <i/>
         - <b> <i> Parameters: </i> </b>
             - None
+    - <i> create_common_encounter(pokemon_name, pokemon_type, rarity): </i> This method will create a "common" encounter with a given rarity. The higher the "rarity" value, the more times the encounter will be entered into the encounter table (for instance, create_common_encounter("Bulbasaur", "Grass", 2) will create two entries of "Bulbasaur" in the "Grass" row of the table.
+        - <b> <i> Parameters: </i> </b>
+            - <b> pokemon_name: </b> the name of the Pokemon to be added to the table. The program does not check if you spelled it correctly (but it does check for cases), so please make sure you have spelled it right!
+            - <b> pokemon_type: </b> the type of the Pokemon (where it will be added in the encounter table). The program does not check if you spelled it correctly (but it does check for cases), so please make sure you have spelled it right! It will generate an error if the Type that you specified does not exist.
+            - <b> rarity: </b> the relative rarity of the Pokemon. The higher the rarity, the more times the Pokemon will be entered into the Encounter Table.
+        - <b> <i> Sample Output: </i> </b> `Successfully added Bulbasaur (Rarity 2) to table row Grass.`
+    - <i> create_rare_encounter(pokemon_name, pokemon_type): </i> This method will create a "rare" encounter with a rarity value of 1 (the most uncommon). 
+        - <b> <i> Parameters: </i> </b>
+            - <b> pokemon_name: </b> the name of the Pokemon to be added to the table. The program does not check if you spelled it correctly (but it does internally account for case differences), so please make sure you have spelled it right!
+            - <b> pokemon_type: </b> the type of the Pokemon (where it will be added in the Encounter Table). The program does not check if you spelled it correctly (but it does check for cases), so please make sure you have spelled it right! It will return an error message if the type that you specified does not exist.
+        - <b> <i> Sample Output: </i> </b> `Successfully added rare Greninja to table row Dark.`
+    - <i> remove_encounter(pokemon_to_remove, num_to_remove): </i> This method will remove a specified number entries of a specific Pokemon from the table. If the number specified is more than the number of entries that exist within the table, all entries will be removed.
+        - <b> <i> Parameters: </i> </b>
+            - <b> pokemon_to_remove: </b> the name of the Pokemon to be removed from the table. The program does not check if you spelled it correctly (but it does internally account for case differences), so please make sure you have spelled it right (or if you <i> did </i> make a spelling mistake when adding a Pokemon, use that same spelling mistake to remove it).
+            - <b> num_to_remove: </b> the number of entries of "pokemon_to_remove" to remove from the table. The program will not remove anything from the table if zero or a negative number is inputted.
+        - <b> <i> Sample Output: </i> </b> `Successfully removed 2 instances of Audino from table row Normal.`
+    - <i> generate_team(num_mons): </i> This method generates a random team for a Trainer based on the contained encounter table.
+        - <b> <i> Parameters: </i> </b>
+            - <b> num_mons: </b> the number of Pokemon to be generated. This generation method picks randomly from any type listed in the encounter table at an equal rate (ie: it is equally likely to pick an Electric or a Flying Pokemon, if they are both valid type rows).
         - <b> <i> Sample Output: </i> </b> `Generated Team:  ['Manectric', 'Clefairy', 'Ninetales', 'Vulpix']`
-    - <i> print_type_matrix(): </i> This method prints out the type matrix by type for ease of viewing.
+    - <i> generate_monotype_team(type_of_mon, num_mons): </i> This method generates a random team of Pokemon for a Trainer based on the contained encounter table. Only the specified type will be considered for generation.
         - <b> <i> Parameters: </i> </b>
-        - <b> <i> Sample Output: (One Row) </i> </b>  `Fairy: ["Azumarill", "Clefairy", "Granbull", "Mawile", "Sylveon", "Togetic"]`
+            - <b> type_of_mon: </b> the type of Pokemon to be generated. If the specified type does not exist within the encounter table, the program will return an error message.
+            - <b> num_mons: </b> the number of Pokemon to be generated. This generation method picks only from the specified type.
+        - <b> <i> Sample Output </i> </b> `Generated Team: ['Manectric', 'Emolga', 'Pikachu']`
+    - <i> print_encounter_table(): </i> This method prints out the current state of the encounter table in an easy-to-read format.
+        - <b> <i> Parameters: </i> </b>
             - None
+        - <b> <i> Sample Output: (One Row) </i> </b>  `Fairy: ["Azumarill", "Clefairy", "Granbull", "Mawile", "Sylveon", "Togetic"]`
                 
 ***********************
 
 <b> How to use: </b>
-- `rng = PTU_RNG_Bot(types, type_matrix)` instantiates the bot with "types" and "type_matrix"
-- `rng.initialize_common_rarities()` initializes rarities of all Pokemon as "common"
-- `rng.create_common_encounter("Bulbasaur", "Grass", 2)` inserts common Pokemon "Bulbasaur" to matrix row "Grass"
-- `rng.create_rare_encounter("Greninja", "Dark")` inserts rare Pokemon "Greninja" to matrix row "Dark"
+- `rng = PTU_RNG_Bot(types, encounter_table)` instantiates the bot with Types of "types" and Encounter Table of "encounter_table"
+- `rng.initialize_common_rarities()` initializes rarities of all Pokemon currently within the encounter table as "common" (Rarity 2).
+- `rng.create_common_encounter("Bulbasaur", "Grass", 2)` inserts common Pokemon "Bulbasaur" to table row "Grass" with a Rarity of 2
+- `rng.create_rare_encounter("Greninja", "Dark")` inserts rare Pokemon "Greninja" to table row "Dark"
+- `rng.remove_encounter("Bibarel", "Water", 2)` removes up to 2 instances of Bibarel from the Water row of the encounter table, provided Water is a Type specified within the encounter table and Bibarel exists within it.
 - `rng.generate_team(4)` generates a random team of 4 Pokemon
-- `rng.print_type_matrix()` prints out the current Type Matrix
+- `rng.generate_monotype_team("Water", 4)` generates a random team of 4 Water-Type Pokemon, if Water is a Type specified with the encounter table.
+- `rng.print_encounter_table()` prints out the current encounter table
